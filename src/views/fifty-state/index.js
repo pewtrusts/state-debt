b8101d327d83d3cebc4eff01ed3fe967ff40e1a5
John Osterman <josterman@pewtrusts.org>
2019-03-04 16:54:29 -0500

    Adds updateGroup method to fifty states; needsd FLIP

--- src/views/fifty-state/index.js
+++ src/views/fifty-state/index.js
@@ -30,19 +30,16 @@ export default class FiftyStateView extends Element {
     
     prerender(){
          //container
-        var view = super.prerender(),
-            field = 'debt_percent_SPI';
+        var view = super.prerender();
+        this.field = 'debt_percent_SPI';
         this.bars = [];
         this.groupByFn = this.groupBy !== null ? d => d[this.groupBy] : d => d !== null;
         this.selections = this.parent.createComponent(this.model, Selections, `div.js-fifty-state-selections`, {parent: this});
         this.groupBy = null; // TODO: should this be in the constructor?
         this.nestedData = d3.nest().key(this.groupByFn).sortKeys(ascending()).entries(this.model.data);
         console.log(this.nestedData);
-        this.nestedData.forEach(group => {
-            group.values.forEach(d => {
-                this.bars.push(this.parent.createComponent(this.model, Bar, `div.bar-state-${d.code}`, {parent: this, data: {d,field, color:2}}));
-            });
-        });
+        this.pushBars();
+        
         this.children.push(this.selections, ...this.bars);
 
         if ( this.prerendered && !this.rerender) {
@@ -56,27 +53,38 @@ export default class FiftyStateView extends Element {
        
         return view;
     }
+    pushBars(){
+        this.bars.length = 0;
+        this.nestedData.forEach(group => {
+            group.values.forEach(d => {
+                this.bars.push(this.parent.createComponent(this.model, Bar, `div.bar-state-${d.code}`, {parent: this, data: {d,field: this.field, color:2}}));
+            });
+        });    
+    }
     renderSelections(){
         this.container.appendChild(this.selections.el);
     }
     renderCharts(){
-        var container = document.createElement('div');
+        var container = document.createElement('div'),
+            index = 0;
         container.classList.add('js-fifty-chart-container');
 
         this.nestedData.forEach(group => {
             var groupDiv = document.createElement('div');
             groupDiv.classList.add(s.groupDiv);
             groupDiv.innerHTML = this.groupBy !== null ? `<h3 class="${s.groupHeader}">${this.model.dict[this.groupBy] !== undefined ? this.model.dict[this.groupBy][group.key] : group.key}</h3>` : '';
-            group.values.forEach((d,i) => {
+            group.values.forEach(d => {
                 var barContainer = document.createElement('div');
                 barContainer.classList.add(s.barContainer);
+                barContainer.id = 'barContainer-' + d.code;
                 var label = document.createElement('p');
                 label.classList.add(s.barLabel);
                 label.innerHTML = d.state;
                 
                 barContainer.appendChild(label);
-                barContainer.appendChild(this.bars[i].el);
+                barContainer.appendChild(this.bars[index].el);
                 groupDiv.appendChild(barContainer);
+                index++;
             });
             container.appendChild(groupDiv);
         });
@@ -86,7 +94,10 @@ export default class FiftyStateView extends Element {
     init(){
         PS.setSubs([
             ['field', (msg,data) => {
-                this.update(msg,data);
+                this.updateBars(msg,data);
+            }],
+            ['group', (msg,data) => {
+                this.updateGroups(msg,data);
             }]
         ]);
         console.log('init FiftyStateView', this);
@@ -94,11 +105,22 @@ export default class FiftyStateView extends Element {
             child.init();
         });
     }
-    update(msg,data){
+    updateBars(msg,data){
+        this.field = data;
         this.bars.forEach(bar => {
             bar.data.field = data;
             bar.update();
         });
+    }
+    updateGroups(msg, data){
+        console.log(msg,data,this);
+        this.groupBy = data;
+        this.nestedData = d3.nest().key(this.groupByFn).sortKeys(ascending()).entries(this.model.data);
+        this.pushBars();        
+        this.el.innerHTML = '';
+        this.el.appendChild(this.renderCharts());
+
+        /* TODO ***** FLIP animation and delay group labels (?) */
     }
     
 }
