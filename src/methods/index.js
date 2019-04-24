45ecf6d22daab1edf49bdd2419ac9adc6c73de94
John Osterman <josterman@pewtrusts.org>
2019-04-24 09:26:23 -0400

    Fixes debt per captia decimals

--- src/methods/index.js
+++ src/methods/index.js
@@ -1,17 +1,22 @@
 export function formatValue(match, field){
-    var style = this.model.types.find(d => d.field === field).type,
+    var metaData = this.model.types.find(d => d.field === field),
+        style = metaData.type,
+        decimals = metaData.decimals !== undefined ? metaData.decimals : style === 'number' ? 1 : style === 'currency' ? 2 : style === 'percent' ? 1 : undefined,
         value = match[field],
         formattedValueString;
-    console.log(value);
     switch(style){
         case 'number':
-            formattedValueString = value.toLocaleString('en-US', {minimumFractionDigits: 1});
+            formattedValueString = value.toLocaleString('en-US', {minimumFractionDigits: decimals});
             break;
         case 'currency':
-            formattedValueString = value.toLocaleString('en-US', {style,currency:'USD'});
+            if ( decimals === 0 ){
+                formattedValueString = Math.round(value).toLocaleString('en-US', {style,currency:'USD'}).slice(0,-3);
+            } else {
+                formattedValueString = value.toLocaleString('en-US', {style,currency:'USD'});
+            }
             break;
         case 'percent':
-            formattedValueString = value.toLocaleString('en-US', {style, minimumFractionDigits: 1});
+            formattedValueString = value.toLocaleString('en-US', {style, minimumFractionDigits: decimals});
             break;
         default:
             formattedValueString = '[unformatted]' + value;
